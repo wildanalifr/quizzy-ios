4d9ea58a1a765e4fb3d82a0fefbe29ece6de0d4b
Wildan <wildanalifrio@gmail.com>
2025-02-22 09:58:19 +0700

    Initial Commit

---
+++ quizzy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizzy
+//
+//  Created by Wildan on 22/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
